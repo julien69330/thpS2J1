40ceadc0d273662b92257f68cba98306a8ad63ff
Julien DEBLAERE <j.deblaere69@gmail.com>
2021-10-04 11:20:02 +0200

    commit de tous les exercices fonctionnels

---
+++ exo_10.rb
@@ -0,0 +1,3 @@
+puts "En quelle année es-tu né.e ?"
+birth_year = gets.chomp.to_i
+puts "Oh, tu avais donc #{2017 - birth_year} ans en 2017"
---
+++ exo_11.rb
@@ -0,0 +1,4 @@
+puts "Donne moi un nombre entier"
+number = gets.chomp.to_i
+number.times do puts "Salut ça farte?"
+end
